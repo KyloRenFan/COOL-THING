7e052dfeebe9edb62e716388aca407ee5f87bdb1
KyloRenFan <pduncan107@gmail.com>
2016-01-22 18:44:45 -0500

    Update Screen.java

--- src/data/Screen.java
+++ src/data/Screen.java
@@ -10,12 +10,12 @@ JFrame f = new JFrame();
 f.setSize(500, 700);
 f.setVisible(true);
 f.setDefaultCloseOperation(3);
-JButton b = new JButton("START MULTIPLAYER");
-b.setSize(20, 30);
-JButton b2 = new JButton("START SINGLEPLAYER");
-b2.setSize(20, 30);
+JButton b = new JButton("START MULTIPLAYER GAME");
+b.setSize(15, 40);
+JButton b2 = new JButton("START SINGLEPLAYER GAME");
+b2.setSize(15, 40);
 JButton b3 = new JButton("INSTRUCTIONS");
-b3.setSize(20, 30);
+b3.setSize(15, 40);
 
 f.add(b3);
 f.add(b2);
