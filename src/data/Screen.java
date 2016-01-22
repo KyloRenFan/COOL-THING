cbc37697377a76f4c817d3073545b6b21ff17b16
KyloRenFan <pduncan107@gmail.com>
2016-01-22 12:41:19 -0500

    Update Screen.java

--- src/data/Screen.java
+++ src/data/Screen.java
@@ -1,6 +1,12 @@
 public class Screen{
 
 public Screen(){
+JFrame f = new JFrame();
+JButton b = new JButton("START");
+JFrame f2 = new JFrame();
+JButton b2 = new JButton("INSTRUCTIONS")
+
+  }
 
 }
 
