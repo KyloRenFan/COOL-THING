b3836ce02f13e8d49a46f79d76b2b65c4400db23
KyloRenFan <pduncan107@gmail.com>
2016-01-22 12:45:36 -0500

    Update Screen.java

--- src/data/Screen.java
+++ src/data/Screen.java
@@ -2,10 +2,9 @@ public class Screen{
 
 public Screen(){
 JFrame f = new JFrame();
-JButton b = new JButton("START");
-JFrame f2 = new JFrame();
-JButton b2 = new JButton("INSTRUCTIONS")
-
+JButton b = new JButton("START MULTIPLAYER");
+JButton b2 = new JButton("START SINGLEPLAYER")
+JButton b3 = new JButton("INSTRUCTIONS")
   }
 
 }
